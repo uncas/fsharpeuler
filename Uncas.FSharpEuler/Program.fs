73b003e42c33a55869488f65c79afdb6638454d3
Ole Lynge Soerensen <osoerensen@ebay.com>
2016-09-03 09:44:34 +0200

    Use fibonacci class.

--- Uncas.FSharpEuler/Program.fs
+++ Uncas.FSharpEuler/Program.fs
@@ -1,6 +1,6 @@
-﻿type private Fibonacci(value, next) =
+﻿type Fibonacci(value, next) =
     member this.Value = value
-    member this.Next() =
+    member this.Next =
         Fibonacci(next, value + next)
     static member First = Fibonacci(1, 2)
 
@@ -25,12 +25,12 @@ let numberIfEven number =
     if isMultipleOf number 2 then number
     else 0
 
-let rec addNextEvenFibonacci fib1 fib2 max =
-    if fib2 > max then 0
-    else numberIfEven fib2 + addNextEvenFibonacci fib2 (fib1+fib2) max
+let rec addNextEvenFibonacci (fib:Fibonacci) max =
+    if fib.Value > max then 0
+    else numberIfEven fib.Value + addNextEvenFibonacci fib.Next max
 
 let euler2 =
-    addNextEvenFibonacci 1 2 4000000
+    addNextEvenFibonacci Fibonacci.First 4000000
 
 let euler actual expected =
     printfn "(%A) %A should be %A" (actual = expected) actual expected
@@ -39,5 +39,4 @@ let euler actual expected =
 let main argv = 
     euler euler1 233168
     euler euler2 4613732
-    printfn "(%A)" (Fibonacci.First.Next().Value)
     0 // return an integer exit code
